598c9f38cb16b6db1704c2aae6d7f914378aeb2e
zeshan <zeshan.14jul1789@gmail.com>
2023-09-05 15:31:54 -0600

    Added another variable which will tracks the screen width and if it is below 1024, then it will toggle. It is responsible for what logo gets displayed where the large logo will be used in the collapsed sidebar and if the screen is more narrow than 1024. Required use effect to add an event listener when the window is resized. The function just runs a check if the browser is more narrow than 1024.

--- features/layout/sidebar-navigation/sidebar-navigation.tsx
+++ features/layout/sidebar-navigation/sidebar-navigation.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { Routes } from "@config/routes";
 import classNames from "classnames";
 import { NavigationContext } from "./navigation-context";
@@ -19,7 +19,19 @@ const menuItems = [
 export function SidebarNavigation() {
   const router = useRouter();
   const { isSidebarCollapsed, toggleSidebar } = useContext(NavigationContext);
-  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [ isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [ isMobileDisplay, setMobileDisplay] = useState(false);
+  
+  useEffect(() => {
+    function handleResize(){
+      //setMobileMenuOpen(window.innerWidth < 1024); 
+      setMobileDisplay(window.innerWidth < 1024);
+    }
+
+    setMobileDisplay(window.innerWidth < 1024);
+    window.onresize = handleResize;
+    });
+
   return (
     <div
       className={classNames(
@@ -37,8 +49,8 @@ export function SidebarNavigation() {
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
             src={
-              isSidebarCollapsed
-                ? "/icons/logo-small.svg"
+              isSidebarCollapsed 
+                ? (isMobileDisplay? "/icons/logo-large.svg" : "icons/logo-small.svg") 
                 : "/icons/logo-large.svg"
             }
             alt="logo"
