442731f7acd8919c9689ecbeb52348063e7c494c
zeshan <zeshan.14jul1789@gmail.com>
2023-09-06 11:22:19 -0600

    Added an index to export footer component. Also added the file which willimpliment the footer component, currenlty skeleton code.

---
+++ features/layout/footer/footer.tsx
@@ -0,0 +1,13 @@
+import {} from "react";
+import styles from "./footer.module.scss";
+
+
+export function footer() {
+    console.log("Dummy footer for testing.");
+    return (
+        <div>
+            <h1>"Hi I'm the Footer"</h1>
+            {/* TODO */}
+        </div>
+    );
+}
