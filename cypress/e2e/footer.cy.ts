624933b29057d26db95d209fc8714ad14320c0b3
zeshan <zeshan.14jul1789@gmail.com>
2023-09-08 18:54:19 -0600

    Added an end to end test for cypress. Checking each page to make sure the footer renders and contains appropriate content.

--- cypress/e2e/footer.cy.ts
+++ cypress/e2e/footer.cy.ts
@@ -1,5 +1,22 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+const address = "http://localhost:3000/";
+const pages = ["dashboard","dashboard/issues", "dashboard/alerts", "dashboard/users", "dashboard/settings"];
+
+function footerContentAssert(){
+  cy.get("footer")
+    .contains("Docs")
+  cy.get("footer")
+    .contains("API")
+  cy.get("footer")
+    .contains("Help")
+  cy.get("footer")
+    .contains("Community");
+}
+
+describe('Footer', () => {
+  pages.forEach(
+    (page) => 
+      it("Should visit", () => 
+        cy.visit(address + page)
+        .then(footerContentAssert)
+      )
+  )});
