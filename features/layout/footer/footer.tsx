74f7b5deede95c07dfc484108c2eb0e0c7a2a574
zeshan <zeshan.14jul1789@gmail.com>
2023-09-09 11:03:11 -0600

    replaecd an inline require statement with proper import for the version number in the footer.

--- features/layout/footer/footer.tsx
+++ features/layout/footer/footer.tsx
@@ -1,12 +1,13 @@
 import {} from "react";
 import styles from "./footer.module.scss";
 import classNames from "classnames";
+import projectInfo from "package.json";
 
 export function Footer() {
   return (
     <footer className={classNames(styles.container)}>
       <div className={styles.version}>
-        <p> {"Version: " + require("package.json").version} </p>
+        <p> {"Version: " + projectInfo.version} </p>
       </div>
 
       <div className={styles.links}>
