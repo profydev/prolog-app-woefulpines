e24742fcc06033e180263139e557a3aeb8157074
zeshan <zeshan.14jul1789@gmail.com>
2023-09-18 14:07:30 -0600

    Added the svg manually as I couldn't get the arrow to change to the proper color.

--- features/ui/error-screen/error-screen.tsx
+++ features/ui/error-screen/error-screen.tsx
@@ -17,7 +17,10 @@ export function ErrorScreen(/**{ theError } : ErrorScreenProps **/){
 
                 <div className={styles.button}>
                     <div>Try again.</div>
-                    <img className={styles.arrow} src="/icons/arrow-right.svg"/>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="#B42318"   xmlns="http://www.w3.org/2000/svg" transform="rotate(180)">
+                    <path d="M19 12H5M5 12L12 19M5 12L12 5" stroke="#B42318" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+
                 </div>
             </div>
         </div>
