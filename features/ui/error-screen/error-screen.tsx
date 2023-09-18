ce97256f13230ee8d860d8507fce4f0c53da6614
zeshan <zeshan.14jul1789@gmail.com>
2023-09-18 13:54:38 -0600

    Added an error screen component we can use for any erros in fetching data.

--- features/ui/error-screen/error-screen.tsx
+++ features/ui/error-screen/error-screen.tsx
@@ -1,13 +1,25 @@
 import styles from "./error-screen.module.scss";
+import { Button } from "../button";
 
 type ErrorScreenProps = {
     theError : Error
 };
 
-export function ErrorScreen({ theError } : ErrorScreenProps ){
+export function ErrorScreen(/**{ theError } : ErrorScreenProps **/){
     return(
-        <div>
-            Error: { theError.message }
+        <div className={styles.errorContainer}>
+            <img className={styles.alert} src="/icons/alert-circle.svg"/>
+
+            <div className={styles.info}>
+                <div>
+                    There was a problem while loading the project data
+                </div>
+
+                <div className={styles.button}>
+                    <div>Try again.</div>
+                    <img className={styles.arrow} src="/icons/arrow-right.svg"/>
+                </div>
+            </div>
         </div>
     );
 }
