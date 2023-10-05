aafc96b683d9ed7b4f70fe4bc5cf0e1c1dc33ff2
zeshan <zeshan.14jul1789@gmail.com>
2023-10-04 18:58:02 -0600

    Added the enums and basic skeleton of the button componenet.

--- features/ui/button/button.tsx
+++ features/ui/button/button.tsx
@@ -2,8 +2,37 @@ import { ButtonHTMLAttributes } from "react";
 import classNames from "classnames";
 import styles from "./button.module.scss";
 
-export function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
+export enum ButtonSize{
+  sm = "sm",
+  md = "md", 
+  lg = "lg", 
+  xl = "xl"
+}
+
+export enum TextPlacement{
+  leading   = "leading",
+  trailing  = "trailing", 
+  noText    = "noText"
+}
+
+export enum ButtonColor{
+  primary     = "primary", 
+  secondary   = "secondary",
+  gray        = "gray", 
+  empty       = "empty",
+  emptyGray   = "emptyGray" 
+}
+
+type ButtonProps = {
+  buttonSize      : ButtonSize; 
+  buttonColor     : ButtonColor;
+} & ButtonHTMLAttributes<HTMLButtonElement>; 
+
+export function Button({buttonSize, buttonColor, ...props} : ButtonProps){
   return (
-    <button {...props} className={classNames(styles.button, props.className)} />
+    <button {...props} className={classNames(
+            styles.button, 
+            styles[buttonSize],
+            styles[buttonColor])}/>
   );
 }
