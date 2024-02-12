d0620ad68d8d4558b01a6f8d018216fcc7c9a7a2
zeshan <zeshan.14jul1789@gmail.com>
2024-02-12 14:27:34 -0600

    Added fields related to the text placement in the button.

--- features/ui/button/button.stories.tsx
+++ features/ui/button/button.stories.tsx
@@ -1,21 +1,28 @@
 import React from "react";
 import { Meta, StoryFn } from "@storybook/react";
-import { Button, ButtonColor, ButtonSize } from "./button";
-
+import { Button, ButtonColor, ButtonSize, TextPlacement } from "./button";
 
 export default {
-    title: "UI/Button",
-    component: Button,
-    parameters: {
-      // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
-      layout: "fullscreen",
-    },
-  } as Meta<typeof Button>;
+  title: "UI/Button",
+  component: Button,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "fullscreen",
+  },
+} as Meta<typeof Button>;
 
-
-  const Template: StoryFn<typeof Button> = ({ buttonSize, buttonColor, children }) => (
+const Template: StoryFn<typeof Button> = ({
+  buttonSize,
+  buttonColor,
+  textPlacement,
+  children,
+}) => (
   <div style={{ padding: 50 }}>
-    <Button buttonColor={buttonColor} buttonSize={buttonSize}>
+    <Button
+      buttonColor={buttonColor}
+      buttonSize={buttonSize}
+      textPlacement={textPlacement}
+    >
       {children}
     </Button>
   </div>
@@ -25,6 +32,7 @@ export const Default = Template.bind({});
 Default.args = {
   buttonSize: ButtonSize.sm,
   buttonColor: ButtonColor.primary,
+  textPlacement: TextPlacement.noText,
   children: "Label",
 };
 
--- features/ui/button/button.tsx
+++ features/ui/button/button.tsx
@@ -2,40 +2,50 @@ import { ButtonHTMLAttributes } from "react";
 import classNames from "classnames";
 import styles from "./button.module.scss";
 
-export enum ButtonSize{
+export enum ButtonSize {
   sm = "sm",
-  md = "md", 
-  lg = "lg", 
-  xl = "xl"
+  md = "md",
+  lg = "lg",
+  xl = "xl",
 }
 
-export enum TextPlacement{
-  leading   = "leading",
-  trailing  = "trailing", 
-  noText    = "noText"
+export enum TextPlacement {
+  leading = "leading",
+  trailing = "trailing",
+  noText = "noText",
 }
 
-export enum ButtonColor{
-  primary     = "primary", 
-  secondary   = "secondary",
-  gray        = "gray", 
-  empty       = "empty",
-  emptyGray   = "emptyGray" 
+export enum ButtonColor {
+  primary = "primary",
+  secondary = "secondary",
+  gray = "gray",
+  empty = "empty",
+  emptyGray = "emptyGray",
 }
 
 type ButtonProps = {
-  children        : React.ReactNode;
-  buttonSize      : ButtonSize;
-  buttonColor     : ButtonColor;
+  children: React.ReactNode;
+  buttonSize: ButtonSize;
+  buttonColor: ButtonColor;
+  textPlacement: TextPlacement;
 };
 
-export function Button({children, buttonSize, buttonColor} : ButtonProps){
+export function Button({
+  children,
+  buttonSize,
+  buttonColor,
+  textPlacement,
+}: ButtonProps) {
   return (
-    <button className={classNames(
-            styles.button, 
-            styles[buttonSize],
-            styles[buttonColor])}>
-    {children}
+    <button
+      className={classNames(
+        styles.button,
+        styles[buttonSize],
+        styles[buttonColor],
+        styles[textPlacement],
+      )}
+    >
+      {children}
     </button>
   );
 }
