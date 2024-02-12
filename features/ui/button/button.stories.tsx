44de319c66efe537cee56e41f0a47575b858f8f3
zeshan <zeshan.14jul1789@gmail.com>
2024-02-12 12:56:19 -0600

    Added the code to set up a basic story book entry for my custom button component.

--- features/ui/button/button.stories.tsx
+++ features/ui/button/button.stories.tsx
@@ -1,3 +1,33 @@
 import React from "react";
 import { Meta, StoryFn } from "@storybook/react";
 import { Button, ButtonColor, ButtonSize } from "./button";
+
+
+export default {
+    title: "UI/Button",
+    component: Button,
+    parameters: {
+      // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+      layout: "fullscreen",
+    },
+  } as Meta<typeof Button>;
+
+
+  const Template: StoryFn<typeof Button> = ({ buttonSize, buttonColor, children }) => (
+  <div style={{ padding: 50 }}>
+    <Button buttonColor={buttonColor} buttonSize={buttonSize}>
+      {children}
+    </Button>
+  </div>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  buttonSize: ButtonSize.sm,
+  buttonColor: ButtonColor.primary,
+  children: "Label",
+};
+
+Default.parameters = {
+  viewMode: "docs",
+};
